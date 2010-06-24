f2f2079aa5a37be236b0398e78a2ac8b4d658159
James Starmer <jps@starmer.com>
2010-06-23 21:12:45 -0400

    Devise is working now.

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "warden", :version => "0.5.1"
-  config.gem "devise", :version => "0.4.1"
+  config.gem "warden"
+  config.gem "devise"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
